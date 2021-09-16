3a56e426c40522cdc22576102294377464ddd615
“Amu-Y” <hakumai.morimori@gmail.com>
2021-09-16 11:52:39 +0000

    [Add]基礎

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+
+  root to: "public#top"
+  get 'about' => 'public#about'
+  get 'instructions' => 'public#instructions'
+  
+  resources :users, only: [:create, :edit, :update, :destroy]
+  resources :workspaces
+  resources :words
 end
