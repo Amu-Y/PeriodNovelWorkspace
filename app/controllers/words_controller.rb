4e010fe13ac73fdf8fdeb6bfd6e26c86a0c7894f
“Amu-Y” <hakumai.morimori@gmail.com>
2021-09-29 09:57:01 +0000

    [Finish]単語機能実装

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -3,14 +3,30 @@ class WordsController < ApplicationController
   def create
     @workspace = Workspace.find(params[:workspace_id])
     @word = Word.new(word_params)
+    @word.workspace_id = @workspace.id
+    @word.save
+  end
+
+  def show
+    @workspace = Workspace.find(params[:workspace_id])
+    @word = Word.find(params[:word_id])
+  end
+
+  def edit
+    @workspace = Workspace.find(params[:workspace_id])
+    @word = Word.find(params[:word_id])
   end
 
   def update
     @workspace = Workspace.find(params[:workspace_id])
+    @word = Word.find(params[:id])
+    @word.update
   end
 
   def destroy
     @workspace = Workspace.find(params[:workspace_id])
+    @word = Word.find(params[:id])
+    @word.destroy
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,12 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :update, :destroy]
   # 執筆機能
   resources :workspaces do
-    resources :novels, only: [:create, :update, :destroy, :new, :edit, :show,]
-    resources :words, only: [:create, :update, :destroy]
+    resources :novels, only: [:create, :update, :destroy, :new, :edit, :show]
+    resources :words, only: [:create, :update, :destroy, :new] do
+      get "/words/show", to: 'words#show', as: 'show'
+      get "/words/edit", to: 'words#edit', as: 'edit'
+    end
+
   end
-  
+
 end
