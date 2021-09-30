cb648b15da972709afccec5d76ac313bb87017aa
“Amu-Y” <hakumai.morimori@gmail.com>
2021-09-30 11:55:45 +0000

    [add]レイアウト、バリデーション

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -1,6 +1,9 @@
 class Workspace < ApplicationRecord
-  
+
   belongs_to :user
   has_many :novels, dependent: :destroy
   has_many :words, dependent: :destroy
+
+  validates :noveltitle, presence: true
+  validates :overview, presence: true
 end
