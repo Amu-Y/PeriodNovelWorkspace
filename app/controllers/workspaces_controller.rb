867da11c52906d20c90f849992a465ec59f1c956
“Amu-Y” <hakumai.morimori@gmail.com>
2021-10-03 08:59:01 +0000

    [Fix,Add]微修正、一部バリデーションメッセージ実装

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -2,8 +2,11 @@ class WorkspacesController < ApplicationController
   def create
     @workspace = Workspace.new(workspace_params)
     @workspace.user_id = current_user.id
-    @workspace.save
-    redirect_to workspace_path(@workspace.id)
+    if @workspace.save
+      redirect_to workspace_path(@workspace.id)
+    else
+      render "workspaces/new"
+    end
   end
 
   def index
@@ -32,8 +35,12 @@ class WorkspacesController < ApplicationController
 
   def update
     @workspace = Workspace.find(params[:id])
-    @workspace.update(workspace_params)
-    redirect_to workspace_path(@workspace.id)
+    if @workspace.update(workspace_params)
+      redirect_to workspace_path(@workspace.id)
+    else
+      flash[:error] = "小説タイトルとあらすじを1文字以上入力して下さい。"
+      redirect_back fallback_location: { action: "show", id: 5 }
+    end
   end
 
 
